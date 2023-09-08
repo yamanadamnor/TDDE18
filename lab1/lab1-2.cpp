442ab7266818178bb0afc248cb5bc85640106148
Yaman <yamix2000@gmail.com>
2023-09-08 13:02:02 +0200

    fix: Update lab1-2

--- lab1/lab1-2.cpp
+++ lab1/lab1-2.cpp
@@ -3,79 +3,74 @@
 #include <string>
 using namespace std;
 
-template<typename T> void printElement(T t, const int& width) {
-  cout << left << setw(width) << setfill(' ') << t;
-}
-
 int main() {
-  float firstPrice, lastPrice, taxPercent, stride;
+  float firstPrice{};
+  float lastPrice{};
+  float taxPercent{};
+  float stride{};
 
-  cout << "INPUT PART\n==========" << endl;
+  cout << "INPUT PART" << endl;
+  cout << setw(10) << setfill('=') << "" << endl;
 
-  do {
+  while (true) {
     cout << left << setw(18) << setfill(' ') << "Enter first price"  << ": ";
     cin >> firstPrice;
-
-    if (firstPrice < 0) {
+    if (!(firstPrice > 0)) {
       cerr << "ERROR: First price must be at least 0 (zero) SEK" << endl;
+    } else {
+      break;
     }
   }
-  while (!(firstPrice > 0));
 
-  do {
-    cout << setw(18) << setfill(' ') << "Enter last price" << ": ";
+  
+  while (true) {
+    cout << setw(18) << "Enter last price" << ": ";
     cin >> lastPrice;
 
     if (lastPrice < firstPrice || lastPrice < 0) {
       cerr << "ERROR: Last price must be at least 0 (zero) SEK and higher than First price" << endl;
+    } else {
+      break;
     }
   }
-  while (lastPrice < firstPrice || lastPrice < 0);
-
-
-  do {
-    cout << setw(18) << setfill(' ') << "Enter stride" << ": ";
-    cin >> fixed >> setprecision(2) >> stride;
-
-    // Workaround the floating point rounding errors
-    if (stride < 0.009){
+  while (true) {
+    cout << setw(18) << "Enter stride" << ": ";
+    cin >> stride;
+    if (!(stride >= 0.01f)) {
       cerr << "Stride must be atleast 0.01" << endl;
+    } else {
+      break;
     }
   } 
-  // Workaround the floating point rounding errors
-  while (stride < 0.009);
-
-  do {
-    cout << setw(18) << setfill(' ') << "Enter tax percent" << ": ";
+  
+  while (true) {
+    cout << setw(18) << "Enter tax percent" << ": ";
     cin >> taxPercent;
-
     if (taxPercent < 0 || taxPercent > 100) {
       cerr << "tax must be atleast 0 (zero) and at most 100.00" << endl;
-
+    } else {
+      break;
     }
   }
-  while (taxPercent < 0 || taxPercent > 100);
 
-  cout << "\nTAX TABLE\n=========" << endl;
+  cout << "\nTAX TABLE" << endl;
+  cout << setw(9) << setfill('=') << "" << endl;
   cout << right << setw(12) << setfill(' ') << "Price";
-  cout << setw(18) << setfill(' ') << "Tax";
-  cout << setw(20) << setfill(' ') << "Price with tax" << endl;
-
+  cout << setw(18) << "Tax";
+  cout << setw(20) << "Price with tax" << endl;
   cout << setw(50) << setfill('-') << "" << endl;
 
-  float price, tax, priceWithTax;
-  int i = 0;
+  float price{};
+  float tax{};
+  float priceWithTax{};
 
-  while (firstPrice + i*stride < lastPrice) {
+  for (int i{0}; i*stride + firstPrice <= lastPrice; i++) {
     price = firstPrice + i*stride;
     tax = price * (taxPercent/100);
     priceWithTax = price + tax;
 
     cout << setw(12) << setfill(' ') << setprecision(2) << fixed << price;
-    cout << setw(18) << setfill(' ') << setprecision(2) << tax;
-    cout << setw(20) << setfill(' ') << setprecision(2) << priceWithTax << endl;
-
-    i++;
+    cout << setw(18) << setprecision(2) << tax;
+    cout << setw(20) << setprecision(2) << priceWithTax << endl;
   }
-
 }
