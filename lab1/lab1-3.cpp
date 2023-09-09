e4fc4e0ada0ebfa18c81b2226c20b0a339fb00e4
Yaman <yamix2000@gmail.com>
2023-09-09 10:05:54 +0200

    fix: Update lab1-3

--- lab1/lab1-3.cpp
+++ lab1/lab1-3.cpp
@@ -9,43 +9,49 @@ using namespace std;
 int main()
 {
   fstream fs;
-  string word;
-  string longest = "";
-  string shortest = "";
-  float average = 0.0;
-  int wordCount = 0;
+  string word{""};
+  string longest{""};
+  string shortest{""};
+  float wordLengthSum{0.0};
+  int wordCount{0};
 
   fs.open("input.txt");
 
   if (!fs)
   {
-    cout << "No such file exists" << endl;
+    cerr << "No such file exists" << endl;
+    return 0;
   }
-  else
+
+  if (fs.peek() == EOF)
   {
+    cout << "The file provided is empty!" << endl;
+    return 0;
+  }
 
-    while (fs >> word)
+  if (wordCount == 0)
+  {
+    fs >> shortest;
+    wordCount++;
+  }
+
+  while (fs >> word)
+  {
+    if (word.length() > longest.length())
     {
-      if (wordCount == 0)
-      {
-        shortest = word;
-      }
-      if (word.length() > longest.length())
-      {
-        longest = word;
-      }
-      else if (word.length() < shortest.length())
-      {
-        shortest = word;
-      }
-
-      wordCount++;
-      average += word.length();
+      longest = word;
     }
+    else if (word.length() < shortest.length())
+    {
+      shortest = word;
+    }
+
+    wordCount++;
+    wordLengthSum += word.length();
   }
   fs.close();
   cout << "There are " << wordCount << " words in the file." << endl;
   cout << "The shortest word was \"" << shortest << "\" with " << shortest.length() << " charachter(s)" << endl;
   cout << "The longest word was \"" << longest << "\" with " << longest.length() << " charachter(s)" << endl;
-  cout << "The average length was " << setprecision(2) << fixed << average / wordCount << " charachter(s)" << endl;
+  cout << "The average length was " << setprecision(2) << fixed << wordLengthSum / wordCount << " charachter(s)" << endl;
 }
